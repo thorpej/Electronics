189232b30b8f1a208aebb01167f3bb32c3235c3f
Jason Thorpe <thorpej@me.com>
2019-07-20 16:02:39 -0700

    Bump copyright year.

--- GPIO/Shift_Register/sregtest.c
+++ GPIO/Shift_Register/sregtest.c
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2018 Jason R. Thorpe
+Copyright (c) 2018, 2019 Jason R. Thorpe
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
